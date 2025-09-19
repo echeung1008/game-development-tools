719a6cdf54635bceed994dc8d5a89ae9c0dc7f78
Edmund Cheung <echeung1008@gmail.com>
2025-09-19 16:37:38 -0700

    Fixed meta typo.

--- Runtime/Extensions/Vector3Extensions.cs
+++ Runtime/Extensions/Vector3Extensions.cs
@@ -4,7 +4,7 @@ public static class Vector3Extensions
 {
     public static Vector3 ClampMagnitudeBetween(this Vector3 vector3, float min, float max)
     {
-        Vector3 result = Vector3.ClampMagnitude(vector3);
+        Vector3 result = Vector3.ClampMagnitude(vector3, max);
         if (vector3.magnitude < min) result = result.normalized * min;
         return result;
     }
